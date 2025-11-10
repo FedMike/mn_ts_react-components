530be0de0e177044c24e1fdb821d9ad0349823fc
fedmike <molnek87@mail.ru>
2025-11-10 22:48:47 +0900

    Typing React components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todoitem from "./components/TodoItem";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todoitem id='111' completed={false} title='222'/>
     </div>
   );
 }
