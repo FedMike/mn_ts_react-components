228a54d01663f9a0729fda7b71ea713ac4ac942e
fedmike <molnek87@mail.ru>
2025-11-12 19:11:21 +0900

    useRef typing

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,15 @@ import Todoitem from "./components/TodoItem";
 import NewTodoForm from "./components/NewTodoForm";
 
 function App() {
-  const [text, setText] = useState('');
   const [todos, setTodos] = useState<Todo[]>([]);
 
-  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setText(event.target.value);
-  }
-
-  const addTodo = () => {
+  const addTodo = (text: string) => {
     const newTodo: Todo = {
       id: new Date().toString(),
       title: text,
       completed: false,
     }
     setTodos([newTodo, ...todos]);
-    setText('');
   };
 
   useEffect(() => {
@@ -32,7 +26,7 @@ function App() {
 
   return (
     <div className="App">
-      <NewTodoForm value={text} onChange={handleInput} handleClick={addTodo}/>
+      <NewTodoForm handleClick={addTodo}/>
       <Todoitem id='111' completed={false} title='firsttodo' style={{border: '1px solid white'}}/>
     </div>
   );
